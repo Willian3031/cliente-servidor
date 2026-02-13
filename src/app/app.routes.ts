21ccf8ae0caa44cbc82273b53dc1b1cb2f3dae10
Willian3031 <waherrera3@espe.edu.ec>
2026-02-12 21:22:03 -0500

    Se añade la ruta en app.routes, para la GestionTareas

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,6 @@
 import { Routes } from '@angular/router';
+import { GestionTareas } from './paginas/gestion-tareas/gestion-tareas';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    { path: '', component: GestionTareas }
+];
