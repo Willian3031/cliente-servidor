fd45f7a55ebf479d74bf538c92271c96bd84851a
Willian3031 <waherrera3@espe.edu.ec>
2026-02-12 21:49:37 -0500

    Se añade el formulario reactivo en registro-tarea.ts

--- src/app/componentes/registro-tarea/registro-tarea.ts
+++ src/app/componentes/registro-tarea/registro-tarea.ts
@@ -1,10 +1,11 @@
 import { Component, EventEmitter, Output } from '@angular/core';
 import { Tarea } from '../../modelos/tarea.model';
-import { FormsModule } from '@angular/forms';
+import { FormBuilder, Validators, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-registro-tarea',
-  imports: [FormsModule],
+  standalone: true,
+  imports: [ReactiveFormsModule],
   templateUrl: './registro-tarea.html',
   styleUrls: ['./registro-tarea.scss']
 })
@@ -12,6 +13,23 @@ export class RegistroTarea {
 
   // Este decorador permite enviar información al componente padre
   @Output() tareaRegistrada = new EventEmitter<Tarea>();
+
+  // Definimos el formulario reactivo
+  formulario: any;
+
+  // Creamos el construtor para inyectar el formulario reactivo
+  constructor(private fb: FormBuilder) {
+
+  // Se crea el formulario reactivo utilizando FormBuilder
+  this.formulario = this.fb.group({
+    titulo: ['', [Validators.required, Validators.minLength(3)]],
+    descripcion: ['', [Validators.required, Validators.minLength(5)]],
+    fechaLimite: ['', Validators.required],
+    prioridad: ['media', Validators.required],
+    estado: ['pendiente', Validators.required]
+  });
+}
+
 
   titulo: string = '';
   descripcion: string = '';
