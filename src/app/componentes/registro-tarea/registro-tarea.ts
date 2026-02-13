61c10248c4deaea403d43c88cc12d1c9144ea2c4
Willian3031 <waherrera3@espe.edu.ec>
2026-02-12 20:58:41 -0500

    se crea el componente del registro y se usa @Output y EventEmitter

--- src/app/componentes/registro-tarea/registro-tarea.ts
+++ src/app/componentes/registro-tarea/registro-tarea.ts
@@ -1,11 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
+import { Tarea } from '../../modelos/tarea.model';
 
 @Component({
   selector: 'app-registro-tarea',
-  imports: [],
   templateUrl: './registro-tarea.html',
-  styleUrl: './registro-tarea.scss',
+  styleUrls: ['./registro-tarea.css']
 })
-export class RegistroTarea {
+export class RegistroTareaComponent {
 
+  // Este decorador permite enviar información al componente padre
+  @Output() tareaRegistrada = new EventEmitter<Tarea>();
+
+  titulo: string = '';
+  descripcion: string = '';
+
+  registrar(): void {
+
+    // Validación básica para evitar campos vacíos
+    if (!this.titulo || !this.descripcion) {
+      return;
+    }
+
+    const nuevaTarea: Tarea = {
+      titulo: this.titulo,
+      descripcion: this.descripcion
+    };
+
+    // Emitimos la tarea al componente padre
+    this.tareaRegistrada.emit(nuevaTarea);
+
+    // Limpiamos los campos
+    this.titulo = '';
+    this.descripcion = '';
+  }
 }
+
